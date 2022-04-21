348390f44a0ed4d41f3b566cc20fb8e6962e8bcc
Yavas20 <97404892+Yavas20@users.noreply.github.com>
2022-04-21 22:29:24 +0300

    first work is done

--- src/EmrePractice.java
+++ src/EmrePractice.java
@@ -1,5 +1,7 @@
 public class EmrePractice {
     public static void main(String[] args) {
         System.out.println("This is an awesome code");
+
+        System.out.println("This is also a perfecct code");
     }
 }
