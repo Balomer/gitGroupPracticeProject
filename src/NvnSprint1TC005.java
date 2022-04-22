b5fe123defd616f657226fe623bdc931b1526a0d
nevin <aydingezernevin@gmail.com>
2022-04-22 09:14:40 +0300

    Initial commit

---
+++ src/NvnSprint1TC005.java
@@ -0,0 +1,8 @@
+public class NvnSprint1TC005 {
+    public static void main(String[] args) {
+        System.out.println("Good morning future SDETs");
+        System.out.println("Today is an awesome day to work!");
+
+
+    }
+}
