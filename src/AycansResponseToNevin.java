0d2ed1f344dcae0c45e344ec7395f9f9b55cd056
evinaycan <aycankapazan@gmail.com>
2022-04-22 12:05:42 +0300

    My response to Nevin

---
+++ src/AycansResponseToNevin.java
@@ -0,0 +1,15 @@
+public class AycansResponseToNevin {
+    public static void main(String[] args) {
+
+        System.out.println("Good morning Nevin <3");
+        System.out.println("Yess, It's a great day to work!");
+
+        System.out.println("This is my first code done today, thanks to you :)");
+
+        System.out.println("Well done, HTML is so fun, at least for now");
+
+        System.out.println("Have a good day, see you!");
+
+
+    }
+}
