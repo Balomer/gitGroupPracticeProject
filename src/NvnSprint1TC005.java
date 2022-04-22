3d36ec26f3e8f66442759880ecc96ca8be62c203
nevin <aydingezernevin@gmail.com>
2022-04-22 09:18:54 +0300

    Code 3 is done

--- src/NvnSprint1TC005.java
+++ src/NvnSprint1TC005.java
@@ -6,6 +6,9 @@ public class NvnSprint1TC005 {
         System.out.println("Let's practice a little bit of GitHup Collaboration");
         System.out.println("This is my second awesome code done today");
 
+        System.out.println("And after I push these codes and ask pull request,");
+        System.out.println("I will finish HTML Part 1");
+
 
     }
 }
