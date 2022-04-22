2839548ed3a0efc4be5e8d2fd36c500c9ecc9adb
nevin <aydingezernevin@gmail.com>
2022-04-22 09:17:38 +0300

    Code 2 is done

--- src/NvnSprint1TC005.java
+++ src/NvnSprint1TC005.java
@@ -3,6 +3,9 @@ public class NvnSprint1TC005 {
         System.out.println("Good morning future SDETs");
         System.out.println("Today is an awesome day to work!");
 
+        System.out.println("Let's practice a little bit of GitHup Collaboration");
+        System.out.println("This is my second awesome code done today");
+
 
     }
 }
